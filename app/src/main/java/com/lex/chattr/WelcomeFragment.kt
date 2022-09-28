7c630e56afbec7a76ad69aa7899d4ff1ae3bafcf
lex <lex@users.noreply.github.com>
2022-09-28 23:11:34 +0300

    disable the next button on welcome if there is no username

--- app/src/main/java/com/lex/chattr/WelcomeFragment.kt
+++ app/src/main/java/com/lex/chattr/WelcomeFragment.kt
@@ -1,6 +1,8 @@
 package com.lex.chattr
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -20,8 +22,8 @@ class WelcomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
 
         _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
@@ -32,11 +34,30 @@ class WelcomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        enableNextIfUsernameIsNotEmpty()
+
+        binding.edittextUsername.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                enableNextIfUsernameIsNotEmpty()
+            }
+        })
+
         binding.buttonUsernameNext.setOnClickListener {
             val username = binding.edittextUsername.text.toString()
-            val action = WelcomeFragmentDirections.actionWelcomeFragmentToChannelListFragment(username)
+            val action =
+                WelcomeFragmentDirections.actionWelcomeFragmentToChannelListFragment(username)
             findNavController().navigate(action)
         }
+    }
+
+    private fun enableNextIfUsernameIsNotEmpty() {
+        binding.buttonUsernameNext.isEnabled = binding.edittextUsername.text.toString().isNotBlank()
     }
 
     override fun onDestroyView() {
